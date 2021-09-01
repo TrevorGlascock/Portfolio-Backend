7b94860e4fcd2ff63fb3f3f34360ff1401d9868b
Trevor Glascock <5269652+TrevorGlascock@users.noreply.github.com>
2021-09-01 13:49:33 -0500

    Updated to allow for frontend and backend repo/deploy links

--- src/db/migrations/20210901162937_createProjectsTable.js
+++ src/db/migrations/20210901162937_createProjectsTable.js
@@ -1,14 +1,18 @@
-exports.up = function(knex) {
-    return knex.schema.createTable("categories", (table) => {
+exports.up = function (knex) {
+  return knex.schema.createTable("categories", (table) => {
     table.increments("project_id").primary();
     table.string("project_title");
     table.text("project_description");
-    table.string("project_screenshot")
-    table.string("project_repo")
-    table.string("project_deploy")
-    })
+    table.string("project_screenshot");
+    
+    table.string("frontend_repo");
+    table.string("frontend_deploy");
+
+    table.string("backend_repo");
+    table.string("backend_deploy");
+  });
 };
 
-exports.down = function(knex) {
-  return knex.schema.dropTable("projects")
+exports.down = function (knex) {
+  return knex.schema.dropTable("projects");
 };
