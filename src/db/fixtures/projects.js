2cfcb3dfa0d1c2bff473bd540a7ffd18ddb7a121
Trevor Glascock <5269652+TrevorGlascock@users.noreply.github.com>
2021-09-01 13:50:19 -0500

    Created Project Fixtures file as an easy way to update and maintain the database with seeding

---
+++ src/db/fixtures/projects.js
@@ -0,0 +1,16 @@
+module.exports = [
+  {
+    project_title: "Glasscard Flashcards",
+    project_description:
+      "Full-Stack Flashcard app built from the ground-up in React",
+      
+    project_screenshot:
+      "https://raw.githubusercontent.com/TrevorGlascock/Project_Flashcards/main/Screenshots/Homepage.png",
+
+    frontend_repo: "https://github.com/TrevorGlascock/Project_Flashcards",
+    frontend_deploy: "https://glasscard.vercel.app/",
+
+    backend_repo: null,
+    backend_deploy: null,
+  },
+];
