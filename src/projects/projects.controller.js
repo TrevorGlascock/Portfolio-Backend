d189a4cecad4ae7c72d993895e7b3162e6d07fed
Trevor Glascock <5269652+TrevorGlascock@users.noreply.github.com>
2021-09-02 17:21:35 -0500

    Added read function and projectId param validation middleware to controller

--- src/projects/projects.controller.js
+++ src/projects/projects.controller.js
@@ -1,12 +1,29 @@
 const service = require("./projects.service");
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 
+/**************************** Middleware Functions ****************************/
+async function hasProjectId(req, res, next) {
+  const { projectId } = req.params;
+  const project = await service.read(projectId);
+
+  if (!project) return next({ status: 404, message: "Project not found!" });
+
+  res.locals.project = project;
+  return next();
+}
+
 /**************************** CRUDL Operation Handlers ****************************/
 async function list(req, res) {
   const data = await service.list();
   res.json({ data });
 }
 
+function read(req, res) {
+  const { project: data } = res.locals;
+  res.json({ data });
+}
+
 module.exports = {
   list: asyncErrorBoundary(list),
+  read: [asyncErrorBoundary(hasProjectId), read],
 };
