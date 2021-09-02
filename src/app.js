401cc3f4d6fe2203adf62b330056c68fac8d6333
Trevor Glascock <5269652+TrevorGlascock@users.noreply.github.com>
2021-09-02 17:11:53 -0500

    Implemented routeNotFound and errorHandler to the main app

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ const cors = require("cors");
 const app = express();
 
 const projectsRouter = require("./projects/projects.router");
+const routeNotFound = require("./errors/routeNotFound");
+const errorHandler = require("./errors/errorHandler");
 
 app.use(cors());
 app.use(express.json());
