7b7c407620278d3fb36a872871d714973a5e5c59
Trevor Glascock <5269652+TrevorGlascock@users.noreply.github.com>
2021-09-06 20:03:20 -0500

    Removed default DEV_DATABASE_URL and included a comment as a reminder to anyone cloning the repo

--- knexfile.js
+++ knexfile.js
@@ -3,8 +3,8 @@ const path = require("path");
 require("dotenv").config();
 
 const {
-  DEV_DATABASE_URL = 'postgres://rvutwzsq:qzsSlRA9pOz4On_kVLQwg4UkWRVF1Zd2@chunee.db.elephantsql.com/rvutwzsq',
-
+  // Migrations and Seeds will not work if you do not have a .env that contains the appropriate variables which link to a SQL database.
+  DEV_DATABASE_URL,
   PROD_DATABASE_URL,
 } = process.env;
 
